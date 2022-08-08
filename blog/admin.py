84e50af5a1c4a21ce14db51adc2206fc2f5ca611
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-08 17:46:17 -0300

    Criando o search_fields do admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -5,3 +5,5 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'publish', 'status')
     list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+
