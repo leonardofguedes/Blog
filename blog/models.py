9b039c3642ae1921fbe99d7311e6c3ceb08fe865
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-08 17:15:31 -0300

    Criando a ordenação e o método de representação self.title

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
+    STATUS = (
+        ('não-publicado', 'Não-publicado'),
+        ('publicado', 'Publicado')
+    )
     title = models.CharField(max_length=60)
     slug = models.SlugField(max_length=60, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
@@ -11,3 +15,10 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS, default='não-publicado')
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
