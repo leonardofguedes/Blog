46fb0a604f0c74499bfed7a5bcb95ba73c0aac74
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-08 17:49:00 -0300

    Criando o date_hierarchy do admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -8,3 +8,4 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ('title', 'body')
     prepopulated_fields = {'slug': ('title',)}
     raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
