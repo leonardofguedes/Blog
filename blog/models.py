2b086ddb518822f138e9088fd8f272c79c16d897
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-08 17:09:20 -0300

    Criando os fields title, slug e author.

--- blog/models.py
+++ blog/models.py
@@ -3,4 +3,6 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    pass
+    title = models.CharField(max_length=60)
+    slug = models.SlugField(max_length=60, unique_for_date='publish')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
