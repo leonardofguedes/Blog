2d3c499f1e6fa9bfc1f3f0a6cd0fc9c20adb14ef
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-08 17:48:28 -0300

    Criando o raw_id_fields do admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -7,3 +7,4 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('status', 'created', 'publish', 'author')
     search_fields = ('title', 'body')
     prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
