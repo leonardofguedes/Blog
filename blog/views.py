c2bffe7ba41b39b912063c3b8700ee9ff750b7e2
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-09 15:56:26 -0300

    Desenvolvendo a classe

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,10 @@ from django.views.generic import ListView
 
 
 class PostListView(ListView):
-    ...
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/pages/list.html'
 
 
 def post_detail(request, year, month, day, post):
