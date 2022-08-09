c555bdb8987ab6cad4586a9faf1ca53e3b07cbb4
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-09 13:57:44 -0300

    Corrigindo o status para publicado

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ def post_list(request):
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
-                             status='published',
+                             status='publicado',
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
