579d696a6a44daef2cd19db32dd8aac4f3050678
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-09 11:52:18 -0300

    Eliminando o / da url

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import post_list, post_detail
 
 urlpatterns = [
-    path('/', post_list, name='list'),
+    path('', post_list, name='list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='detail')
 ]
