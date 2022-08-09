ec8034c759b4f0c5a8a67b61820d3583fac2fd80
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-09 10:44:08 -0300

    Criando o path do post detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import post_list
+from .views import post_list, post_detail
 
 urlpatterns = [
     path('/', post_list, name='list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='detail')
 ]
