4896352bed747645b24c9a6ba9948020ae975093
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-10 16:49:34 -0300

    Adicionando a URL de post-share

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from blog.views.post_list_view import *
 from blog.views.detail_view import *
+from blog.views.post_share_view import *
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='detail')
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='detail'),
+    path('<int:post_id>/share/', post_share, name='post_share')
 ]
