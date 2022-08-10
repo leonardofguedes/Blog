b06dcb2e1e907e33ecddeff2c5c53efda5c9ad1e
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-10 14:57:20 -0300

    Refatorando a URL para que receba a view class

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import post_list, post_detail
+from .views import PostListView, post_detail
 
 urlpatterns = [
-    path('', post_list, name='list'),
+    path('', PostListView.as_view(), name='list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='detail')
 ]
