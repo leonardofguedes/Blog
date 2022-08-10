eedbfd341487695b25ec91c750302bb9afc8354e
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-10 16:03:03 -0300

    Adicionando o dotenv

--- setup/wsgi.py
+++ setup/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
-
+load_dotenv()
 application = get_wsgi_application()
