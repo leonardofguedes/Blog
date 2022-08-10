fd9515ed77a481d1871cc6e187339a9f3d1fb821
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-10 15:40:51 -0300

    Criando método post_share

--- blog/views/post_share_view.py
+++ blog/views/post_share_view.py
@@ -6,7 +6,7 @@ from blog.models import Post
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='publicado')
 
-    if request.methodo == 'POST':
+    if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
