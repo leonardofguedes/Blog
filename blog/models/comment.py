f01fe1c77c33780ce5c8de189758e1db91c7f97e
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-11 09:29:41 -0300

    Desenvolvendo o model Comment

--- blog/models/comment.py
+++ blog/models/comment.py
@@ -1,5 +1,17 @@
 from django.db import models
-
+from blog.models.post import Post
 
 class Comment(models.Model):
-    ...
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'Comentário de {self.name} em {self.post}'
