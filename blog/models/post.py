ea529e71cd749d3d7d0662bfd32e957d29908d8e
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-11 14:12:53 -0300

    Adicionando o django_taggit ao model

--- blog/models/post.py
+++ blog/models/post.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -36,3 +37,5 @@ class Post(models.Model):
                                        self.publish.month,
                                        self.publish.day,
                                        self.slug])
+
+    tags = TaggableManager()
