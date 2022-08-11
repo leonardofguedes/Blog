88f274db9af78dedecd9931bbb1d9daefd0a7276
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-11 15:37:44 -0300

    Adicionando na view de detail o context de posts similares com mesmas tags

--- blog/views/detail_view.py
+++ blog/views/detail_view.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models.post import Post
-from blog.models.comment import Comment
 from blog.forms.comment_form import CommentForm
+from django.db.models import Count
 
 
 def post_detail(request, year, month, day, post):
@@ -21,7 +21,11 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+    posts_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=posts_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
     return render(request, 'blog/pages/detail.html', {'post': post,
                                                           'comments':comments,
                                                           'new_comment': new_comment,
-                                                          'comment_form': comment_form})
+                                                          'comment_form': comment_form,
+                                                          'similar_posts': similar_posts})
