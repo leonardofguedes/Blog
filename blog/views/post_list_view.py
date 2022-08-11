9b4a8d6912411eb3a72ab3fd9b20bdb3d71fb405
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-11 15:19:12 -0300

    Refatorando a class view para uma function view e inserindo a tag no context

--- blog/views/post_list_view.py
+++ blog/views/post_list_view.py
@@ -1,9 +1,25 @@
+from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
+from django.shortcuts import get_object_or_404, render
 from blog.models.post import Post
-from django.views.generic import ListView
+from taggit.models import Tag
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/pages/list.html'
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/pages/list.html', {'page': page,
+                                                   'posts': posts,
+                                                   'tag': tag})
