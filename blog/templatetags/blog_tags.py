a886d5725b72eb9a5447beea13b3082314d3cfaf
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-15 17:14:35 -0300

    Commit

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -23,6 +23,7 @@ def get_most_commented_posts(count=3):
     return Post.published.annotate(
         total_comments=Count('comments')).order_by('-total_comments')[:count]
 
+
 @register.filter(name='markdown')
 def markdown_format(text):
     return mark_safe(markdown.markdown(text))
