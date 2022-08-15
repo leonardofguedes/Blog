50e2d9e9c1f10b1ed64e43121e826fabc06fb9dd
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-15 10:56:01 -0300

    Adicionando cover, que depois substituirá post-list

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from blog.views.post_list_view import *
 from blog.views.detail_view import *
 from blog.views.post_share_view import *
+from blog.views.cover import cover
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
 
@@ -12,6 +13,7 @@ sitemaps = {
 
 urlpatterns = [
     path('', post_list, name='list'),
+    path('cover', cover, name='cover'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='detail'),
     path('<int:post_id>/share/', post_share, name='post_share'),
     path('tag/<slug:tag_slug>/', post_list, name='post_list_by_tag'),
