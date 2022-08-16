77517fc4b33746eb218375cd942dc10d1942083f
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-16 09:38:03 -0300

    Alterando a funnção view para utilização da tag no template cover

--- blog/urls.py
+++ blog/urls.py
@@ -16,6 +16,6 @@ urlpatterns = [
     path('cover', cover, name='cover'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='detail'),
     path('<int:post_id>/share/', post_share, name='post_share'),
-    path('tag/<slug:tag_slug>/', post_list, name='post_list_by_tag'),
+    path('tag/<slug:tag_slug>/', cover, name='post_list_by_tag'),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
 ]
