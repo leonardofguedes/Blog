191a2dd73d0b2483d092f900679ce03b9ff50be9
leonardofguedes <leonardo.fguedes@gmail.com>
2022-08-16 16:40:18 -0300

    Alterando o banco de dados para postgres

--- setup/settings.py
+++ setup/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get('SECRET')
     }
 }
 
